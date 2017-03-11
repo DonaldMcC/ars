f93a7e74454f757dba4bba8ce870c04f45bbc4fa
DonaldMcC <donaldm2020@gmail.com>
2017-03-11 23:22:48 +0000

    start on basic controller and sorted hassles with ipynb

--- models/db1.py
+++ models/db1.py
@@ -1,5 +1,11 @@
 
 
 db.define_table('activity',
+                Field('activity', 'string', label='Activity'),
                 Field('forename', 'string', label='First Name'),
-                Field('surname', label='surname'))
+                Field('surname', label='surname'),
+                Field('addr1', label='address 1'),
+                Field('addr2', label='address 2'),
+                Field('addr3', label='address 3'),
+                Field('addr4', label='address 4'),
+                )
