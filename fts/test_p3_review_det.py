0accfbd42e21c4b19b0d226869b5ddcc46c33ef1
DonaldMcC <donaldm2020@gmail.com>
2017-06-15 21:43:49 +0100

    further tests fixed

--- fts/test_p3_review_det.py
+++ fts/test_p3_review_det.py
@@ -24,7 +24,7 @@ class AnswerAction (FunctionalTest):
 
     # think we add dictionary with keys linked to selectiosn and then do same to ratings
     @data((USERS['USER2'], USERS['PASSWORD2'], 'The world is under-achieving', {'fullname': 'DMcC', 'orgtype': 'Cther'}),
-          (USERS['USER3'], USERS['PASSWORD3'], 'Lets get this done'), {'fullname': 'DMcC', 'orgtype': 'Cther'})
+          (USERS['USER3'], USERS['PASSWORD3'], 'Lets get this done', {'fullname': 'DMcC', 'orgtype': 'Cther'}))
     @unpack
     def test_answer_action(self, user, passwd, result, othervals):
         mailstring = user + '@user.com'
@@ -35,18 +35,25 @@ class AnswerAction (FunctionalTest):
         password = self.browser.find_element_by_name("password")
         password.send_keys(passwd)
 
+        submit_button = self.browser.find_element_by_css_selector("#submit_record__row input")
+        submit_button.click()
+        time.sleep(1)
+
+        self.url = ROOT + '/review/newindex'
+        get_browser=self.browser.get(self.url)
+
         draftclick = self.browser.find_element_by_id("selectionDraft")
         draftclick.click()
         sortordclick = self.browser.find_element_by_id("sortorder2 Importance")
         sortordclick.click()
-        filterclick = self.browser.find_element_by_id("filtersScope")
-        filterclick.click()
+        #filterclick = self.browser.find_element_by_id("filtersScope")
+        #filterclick.click()
         filtercategory = self.browser.find_element_by_id("filtersCategory")
         filtercategory.click()
-        filtersdate = self.browser.find_element_by_id("filtersDate")
-        filtersdate.click()
-        scope2 = self.browser.find_element_by_id("view_scope2 Regional")
-        scope2.click()
+        #filtersdate = self.browser.find_element_by_id("filtersDate")
+        #filtersdate.click()
+        #scope2 = self.browser.find_element_by_id("view_scope2 Regional")
+        #scope2.click()
 
         #Select(self.browser.find_element_by_id("viewscope_country")).select_by_visible_text("United Arab Emirates")
         #Select(self.browser.find_element_by_id("viewscope_subdivision")).select_by_visible_text("[Abu Dhabi]")
@@ -58,12 +65,9 @@ class AnswerAction (FunctionalTest):
         #driver.find_element_by_css_selector("tr.daysrow.rowhilite > td.selected.day").click()
         #driver.find_element_by_xpath("//button[@type='submit']").click()
 
-        submit_button = self.browser.find_element_by_css_selector("#submit_record__row input")
+        submit_button = self.browser.find_element_by_xpath("//button[@type='submit']")
         submit_button.click()
-        time.sleep(1)
-
-        self.url = ROOT + '/review/newindex'
-        get_browser=self.browser.get(self.url)
+        time.sleep(3)
 
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn(result, body.text)
